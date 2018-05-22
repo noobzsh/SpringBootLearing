34fd6ef5c552f47e2d7384aabbb0c124f822d552
noobzsh <709264279@qq.com>
2018-05-22 15:53:52 +0800

    application.properties个性化配置文件的使用
    application.properties个性化配置文件的使用

--- SpringBootDemo/src/main/java/com/example/demo/TestController.java
+++ SpringBootDemo/src/main/java/com/example/demo/TestController.java
@@ -1,13 +1,32 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    /*
+    http://localhost:8080/
+    */
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
+    }
+
+
+    /*
+    测试application.propertires文件作用
+    http://localhost:8080/testapplicationpropertires
+    */
+    @Value("${testdemo.name}")
+    private String name;
+    @Value("${testdemo.age}")
+    private int age;
+
+    @RequestMapping("/testapplicationpropertires")
+    public String testapplication(){
+        return name+":"+age;
     }
 }
